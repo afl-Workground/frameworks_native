c49b0781f8b0aef106bf1d0f2f7444a4ad7e1885
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-08-28 11:52:44 +0800

    [PERF] disable hwc composition strategy
    Change-Id: Iee33ca6e84ca37736573cd6f1415a8ae79296413

--- services/surfaceflinger/SurfaceFlinger.cpp
+++ services/surfaceflinger/SurfaceFlinger.cpp
@@ -514,7 +514,7 @@ SurfaceFlinger::SurfaceFlinger(Factory& factory) : SurfaceFlinger(factory, SkipI
     property_get("debug.sf.disable_client_composition_cache", value, "0");
     mDisableClientCompositionCache = atoi(value);
 
-    property_get("debug.sf.predict_hwc_composition_strategy", value, "1");
+    property_get("debug.sf.predict_hwc_composition_strategy", value, "0");
     mPredictCompositionStrategy = atoi(value);
 
     property_get("debug.sf.treat_170m_as_sRGB", value, "0");
